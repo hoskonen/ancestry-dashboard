82933ead14c50f8e425f51241f5b3627a53bb007
Petri Hoskonen <route.electrique@gmail.com>
2026-04-03 13:47:15 +0300

    AD-001-02 add barebones api calls and rest test

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import Fastify from "fastify";
+
+const events: any[] = [];
+
+const app = Fastify({ logger: true });
+
+app.get("/health", async () => {
+  return { status: "ok" };
+});
+
+app.post("/events", async (request, reply) => {
+  const event = request.body;
+
+  events.push(event);
+  console.log('EVENTS: ', events);
+
+  return { success: true };
+});
+
+app.get("/events", async () => {
+  return events;
+});
+
+const start = async () => {
+  try {
+    await app.listen({ port: 3000 });
+    console.log("Server running on http://localhost:3000");
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
